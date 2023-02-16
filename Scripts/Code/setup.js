8810a78c312118973ad7dfabd3233fd00e7c6206
TariqJandaly <tariq.jandaly@gmail.com>
2023-02-16 19:19:24 +0300

    Adding some things to the base?

--- Scripts/Code/setup.js
+++ Scripts/Code/setup.js
@@ -1,3 +1,10 @@
+let a = 0
+
 _Start["setup"] = () => {
+    _["title"] = "The Game"
     console.log("The game have started")
+}
+
+_Update["setup"] = () => {
+    a++
 }
--- Scripts/base.js
+++ Scripts/base.js
@@ -1,8 +1,21 @@
 // ! Global Verps.
 let _ = {}
 
+_["title"] = ""
+
+
+// ! No canvas??. I got you
 _.canvas = document.createElement("canvas")
 _.ctx = _.canvas.getContext("2d")
+document.querySelector("body").appendChild(_.canvas)
+
+// ! Making it look good
+_.canvas.width = innerWidth
+_.canvas.height = innerHeight
+
+document.querySelector("body").style.margin = 0
+document.querySelector("body").style.padding = 0
+document.querySelector("body").style.overflow = "hidden"
 
 // ! Setting up Start functions.
 let _Start = {}
@@ -20,6 +33,14 @@ const Update = () => {
     }
 }
 
+// ! Custom functions?. Not a big deal bud
+function NewEvent(type, callback) {
+    addEventListener(type, callback)
+}
+
+// ! Create for some sore of thing, I guss?
+let title = document.createElement("title")
+
 // ! Setting up functions and make them work
 onload = () => {
     Start()
@@ -27,6 +48,12 @@ onload = () => {
     const Loop =  () => {
         requestAnimationFrame(Loop)
         Update()
+
+
+        // ! Setting up things
+        title.textContent = _["title"]
+
+        document.querySelector("head").appendChild(title)
     }
     Loop()
 }
